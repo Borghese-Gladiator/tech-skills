93467fc4353aebf1b598ea38b3002601114234a8
Timothy Shee <timothy_shee@hotmail.com>
2020-09-21 17:37:22 -0400

    Update displayData.js

--- src/constants/displayData.js
+++ src/constants/displayData.js
@@ -2,10 +2,688 @@
 // can only do it myself (since I haven't set up any backend at all)
 
 export default {
-  React: {
-    name: "React",
-    docLink: "https://reactjs.org/",
-    tag: "Frontend",
-    desc: "JS Frontend library to build UI components with state lifecycles",
-  }
+	"<audio>": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"<figcaption>": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"<figure>": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Agile": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Android": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Angular": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Animation Libraries": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Ant": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"App Performance Test": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Automated Tests": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Babel": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Backlog": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Bash": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Batch Scripts": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Build Tools": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"C": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"C#": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"C++": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"CSS Media Queries": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"CSS3": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"CentOS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Chai": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Code Collab": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Component Libraries": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"D3.js": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Daily Stand Up": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Database": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Declarative Pipelines": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"DevOps (CI/CD)": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Discord": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"EOT": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Executor": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Flexbox": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Fonts": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Framer Motion": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Frontend": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Gatsby.js": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Git": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"GitLab": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Gradle": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Gulp": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"HTML5": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Java": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"JavaScript": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Jenkins": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Jest": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Jira": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Kaban Board": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Lighthouse": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Linux": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Material UI": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Material-UI Theme Spacing": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Maven": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Microsoft Teams": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Mocha": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"MongoDB": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"MongoDB Atlas": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"MongoDB Stitch": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Mongoose": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"MySQL": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Next.js": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Nose": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Nuxt.js": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"OS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"PostgreSQL": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Powershell": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Programming Languages": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Python": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"R": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"React": {
+		"name": "React",
+		"docLink": "https://reactjs.org/",
+		"tag": ["Frontend", "JavaScript"],
+		"desc": "JS Frontend library to build UI components with state lifecycles"
+	},
+	"React Bootstrap": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"React Hooks": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"React Router": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Reactstrap": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Redux": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Responsive Design": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Ruby": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"SASS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"SQL Server": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"SQLite": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"SVG": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Scripted Pipelines": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Selenium": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Semantic UI React": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"SinonJS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Slack": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Sprint Planning": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Sprint Retrospective": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Sprint Review": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Styling": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"TTF": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Trello": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"TypeScript": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"UI Testing": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Ubuntu": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Unit Testing": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"VCS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Vanilla JS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Vue.js": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"WIP Limits": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"WOFF": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"WOFF2": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"WebRTC": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Webpack": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Windows": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"Worker": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"apt-get": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"cron": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"iOS": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"jQuery": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"styled-components": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"undefined": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"unittest": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	},
+	"yum": {
+		"name": "DEFAULT VALUE",
+		"docLink": "https://reactjs.org/",
+		"tag": [],
+		"desc": "DEFAULT DESC"
+	}
 }
