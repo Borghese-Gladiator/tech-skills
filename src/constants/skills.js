aaabb69084d5d9f765451b95cb33bf2b6e3c6e5b
Timothy Shee <timothy_shee@hotmail.com>
2020-09-20 20:42:47 -0400

    Update skills.js

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -13,288 +13,432 @@ export default [
 			},
 			{
 				"id": 6,
-        "name": "JavaScript",
-        "children": [
-          {
-            "id": 7,
-            "name": "TypeScript"
-          },
-          {
-            "id": 7,
-            "name": "D3.js"
-          }
-        ]
-      },
+				"name": "JavaScript",
+				"children": [
+					{
+						"id": 8,
+						"name": "TypeScript"
+					},
+					{
+						"id": 9,
+						"name": "D3.js"
+					}
+				]
+			},
 			{
-				"id": 11,
+				"id": 10,
 				"name": "Ruby"
 			},
 			{
-				"id": 8,
+				"id": 11,
 				"name": "C#"
 			},
 			{
-				"id": 9,
+				"id": 12,
 				"name": "C"
 			},
 			{
-				"id": 10,
+				"id": 13,
 				"name": "C++"
-      },
+			},
 			{
-				"id": 10,
+				"id": 14,
 				"name": "R"
 			}
 		]
 	},
 	{
-		"id": 12,
+		"id": 15,
 		"name": "Frontend",
 		"children": [
 			{
-				"id": 14,
-        "name": "Vanilla JS",
-        "children": [
-					{
-						"id": 17,
-						"name": "HTML5"
-					}
-        ]
-      },
-			{
-				"id": 15,
-				"name": "Styling",
+				"id": 17,
+				"name": "Vanilla JS",
 				"children": [
 					{
-						"id": 17,
-						"name": "CSS3"
-					},
-					{
-						"id": 18,
-						"name": "Flexbox"
-					},
-					{
 						"id": 19,
-						"name": "CSS Media Queries"
-					},
-					{
-						"id": 20,
-						"name": "Fonts",
+						"name": "HTML5",
 						"children": [
 							{
+								"id": 21,
+								"name": "<audio>"
+							},
+							{
 								"id": 22,
-								"name": "EOT"
+								"name": "<figure>"
 							},
 							{
 								"id": 23,
+								"name": "<figcaption>"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": 24,
+				"name": "Styling",
+				"children": [
+					{
+						"id": 26,
+						"name": "CSS3"
+					},
+					{
+						"id": 27,
+						"name": "Flexbox"
+					},
+					{
+						"id": 28,
+						"name": "CSS Media Queries"
+					},
+					{
+						"id": 29,
+						"name": "Fonts",
+						"children": [
+							{
+								"id": 31,
+								"name": "EOT"
+							},
+							{
+								"id": 32,
 								"name": "SVG"
 							},
 							{
-								"id": 24,
+								"id": 33,
 								"name": "TTF"
 							},
 							{
-								"id": 25,
+								"id": 34,
 								"name": "WOFF"
 							},
 							{
-								"id": 26,
+								"id": 35,
 								"name": "WOFF2"
 							}
 						]
 					},
 					{
-						"id": 27,
+						"id": 36,
 						"name": "Responsive Design"
 					},
 					{
-						"id": 28,
+						"id": 37,
 						"name": "SASS"
 					},
 					{
-						"id": 29,
+						"id": 38,
 						"name": "styled-components"
 					},
 					{
-						"id": 30,
+						"id": 39,
 						"name": "Material-UI Theme Spacing"
 					}
 				]
 			},
 			{
-				"id": 31,
+				"id": 40,
 				"name": "React",
 				"children": [
 					{
-						"id": 33,
+						"id": 42,
 						"name": "React Hooks"
-          },
-          {
-						"id": 33,
-						"name": "Redux"
-          },
+					},
 					{
-						"id": 34,
+						"id": 43,
+						"name": "Redux"
+					},
+					{
+						"id": 44,
 						"name": "Jest"
 					},
 					{
-						"id": 34,
+						"id": 45,
+						"name": "Gatsby.js"
+					},
+					{
+						"id": 46,
 						"name": "Next.js"
 					},
 					{
-						"id": 35,
+						"id": 47,
 						"name": "Animation Libraries",
 						"children": [
 							{
-								"id": 37,
+								"id": 49,
 								"name": "Framer Motion"
 							}
 						]
 					},
 					{
-						"id": 38,
+						"id": 50,
 						"name": "Component Libraries",
 						"children": [
 							{
-								"id": 40,
+								"id": 52,
 								"name": "Material UI"
 							},
 							{
-								"id": 41,
+								"id": 53,
 								"name": "Semantic UI React"
 							},
 							{
-								"id": 42,
+								"id": 54,
 								"name": "React Bootstrap"
 							},
 							{
-								"id": 43,
+								"id": 55,
 								"name": "Reactstrap"
 							}
-						],
-          },
-					{
-						"id": 34,
-						"name": "React Router"
+						]
 					},
+					{
+						"id": 56,
+						"name": "React Router"
+					}
 				]
 			},
 			{
-				"id": 46,
+				"id": 57,
 				"name": "Angular"
 			},
 			{
-				"id": 47,
+				"id": 58,
 				"name": "Vue.js",
 				"children": [
 					{
-						"id": 49,
+						"id": 60,
 						"name": "Nuxt.js"
 					}
 				]
 			},
 			{
-				"id": 50,
+				"id": 61,
 				"name": "jQuery"
 			},
 			{
-				"id": 51,
+				"id": 62,
 				"name": "Webpack"
 			},
 			{
-				"id": 52,
+				"id": 63,
 				"name": "Babel"
-      },
-      {
-				"id": 14,
-        "name": "WebRTC",
 			},
+			{
+				"id": 64,
+				"name": "WebRTC"
+			}
 		]
-  },
+	},
 	{
 		"id": 65,
 		"name": "Automated Tests",
 		"children": [
 			{
 				"id": 67,
-        "name": "App Performance Test",
-        "children": [
-          {
-            "id": 67,
-            "name": "Lighthouse"
-          }
-        ]
-      },
-      {
-				"id": 67,
-				"name": "API Testing"
-      }
+				"name": "App Performance Test",
+				"children": [
+					{
+						"id": 69,
+						"name": "Lighthouse"
+					}
+				]
+			},
+			{
+				"id": 70,
+				"name": "UI Testing",
+				"children": [
+					{
+						"id": 72,
+						"name": "Selenium"
+					}
+				]
+			},
+			{
+				"id": 73,
+				"name": "Unit Testing",
+				"children": [
+					{
+						"id": 75,
+						"name": "JavaScript",
+						"children": [
+							{
+								"id": 77,
+								"name": "Mocha"
+							},
+							{
+								"id": 78,
+								"name": "Chai"
+							},
+							{
+								"id": 79,
+								"name": "SinonJS"
+							}
+						]
+					},
+					{
+						"id": 80,
+						"name": "Python",
+						"children": [
+							{
+								"id": 82,
+								"name": "unittest"
+							},
+							{
+								"id": 83,
+								"name": "Nose"
+							}
+						]
+					}
+				]
+			}
 		]
 	},
 	{
-		"id": 53,
+		"id": 84,
 		"name": "Database",
 		"children": [
 			{
-				"id": 55,
+				"id": 86,
 				"name": "MongoDB",
 				"children": [
-          {
-						"id": 57,
-						"name": "Mongoose"
-          },
 					{
-						"id": 57,
+						"id": 88,
+						"name": "Mongoose"
+					},
+					{
+						"id": 89,
 						"name": "MongoDB Atlas"
-          },
-          {
-						"id": 57,
+					},
+					{
+						"id": 90,
 						"name": "MongoDB Stitch"
-          }
+					}
 				]
-      },
+			},
 			{
-				"id": 58,
+				"id": 91,
 				"name": "SQLite"
-      },
-      {
-				"id": 58,
+			},
+			{
+				"id": 92,
 				"name": "MySQL"
 			},
 			{
-				"id": 58,
+				"id": 93,
 				"name": "SQL Server"
 			}
 		]
 	},
 	{
-		"id": 59,
+		"id": 94,
 		"name": "DevOps",
 		"children": [
 			{
-				"id": 61,
-				"name": "Child - 4"
+				"id": 96,
+				"name": "Jenkins",
+				"children": [
+					{
+						"id": 98,
+						"name": "Scripted Pipelines"
+					},
+					{
+						"id": 99,
+						"name": "Declarative Pipelines"
+					},
+					{
+						"id": 100,
+						"name": "Executor"
+					}
+				]
+			},
+			{
+				"id": 101,
+				"name": "Cron"
 			}
 		]
 	},
 	{
-		"id": 62,
+		"id": 102,
 		"name": "Build Tools",
 		"children": [
 			{
-				"id": 64,
-				"name": "Child - 4"
+				"id": 104,
+				"name": "Gradle"
+			},
+			{
+				"id": 105,
+				"name": "Maven"
+			},
+			{
+				"id": 106,
+				"name": "Ant"
+			},
+			{
+				"id": 107,
+				"name": "Webpack"
+			},
+			{
+				"id": 108,
+				"name": "Gulp"
 			}
 		]
 	},
 	{
-		"id": 65,
+		"id": 109,
 		"name": "OS",
 		"children": [
 			{
-				"id": 67,
-				"name": "Child - 4"
+				"id": 111,
+				"name": "Windows",
+				"children": [
+					{
+						"id": 113,
+						"name": "Powershell"
+					},
+					{
+						"id": 114,
+						"name": "Batch Scripts"
+					}
+				]
+			},
+			{
+				"id": 115,
+				"name": "Linux",
+				"children": [
+					{
+						"id": 117,
+						"name": "Bash"
+					},
+					{
+						"id": 118,
+						"name": "CentOS",
+						"children": [
+							{
+								"id": 120,
+								"name": "yum"
+							}
+						]
+					},
+					{
+						"id": 121,
+						"name": "Ubuntu",
+						"children": [
+							{
+								"id": 123,
+								"name": "apt-get"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": 124,
+				"name": "Android"
+			},
+			{
+				"id": 125,
+				"name": "iOS"
 			}
 		]
-  },
+	}
 ]
