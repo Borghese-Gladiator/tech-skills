5f2ac3af9096f52c4c852c4bc34d57587b19d77e
Timothy Shee <timothy_shee@hotmail.com>
2020-09-20 21:23:45 -0400

    ES6 Issues - Switch to Hard Code

--- src/scripts/changeIds.js
+++ src/scripts/changeIds.js
@@ -1,4 +1,529 @@
-import data from '../constants/skills'
+const data = [
+	{
+		"id": 2,
+		"name": "Programming Languages",
+		"children": [
+			{
+				"id": 4,
+				"name": "Java"
+			},
+			{
+				"id": 5,
+				"name": "Python"
+			},
+			{
+				"id": 6,
+				"name": "JavaScript",
+				"children": [
+					{
+						"id": 8,
+						"name": "TypeScript"
+					},
+					{
+						"id": 9,
+						"name": "D3.js"
+					}
+				]
+			},
+			{
+				"id": 10,
+				"name": "Ruby"
+			},
+			{
+				"id": 11,
+				"name": "C#"
+			},
+			{
+				"id": 12,
+				"name": "C"
+			},
+			{
+				"id": 13,
+				"name": "C++"
+			},
+			{
+				"id": 14,
+				"name": "R"
+			}
+		]
+	},
+	{
+		"id": 15,
+		"name": "Frontend",
+		"children": [
+			{
+				"id": 17,
+				"name": "Vanilla JS",
+				"children": [
+					{
+						"id": 19,
+						"name": "HTML5",
+						"children": [
+							{
+								"id": 21,
+								"name": "<audio>"
+							},
+							{
+								"id": 22,
+								"name": "<figure>"
+							},
+							{
+								"id": 23,
+								"name": "<figcaption>"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": 24,
+				"name": "Styling",
+				"children": [
+					{
+						"id": 26,
+						"name": "CSS3"
+					},
+					{
+						"id": 27,
+						"name": "Flexbox"
+					},
+					{
+						"id": 28,
+						"name": "CSS Media Queries"
+					},
+					{
+						"id": 29,
+						"name": "Fonts",
+						"children": [
+							{
+								"id": 31,
+								"name": "EOT"
+							},
+							{
+								"id": 32,
+								"name": "SVG"
+							},
+							{
+								"id": 33,
+								"name": "TTF"
+							},
+							{
+								"id": 34,
+								"name": "WOFF"
+							},
+							{
+								"id": 35,
+								"name": "WOFF2"
+							}
+						]
+					},
+					{
+						"id": 36,
+						"name": "Responsive Design"
+					},
+					{
+						"id": 37,
+						"name": "SASS"
+					},
+					{
+						"id": 38,
+						"name": "styled-components"
+					},
+					{
+						"id": 39,
+						"name": "Material-UI Theme Spacing"
+					}
+				]
+			},
+			{
+				"id": 40,
+				"name": "React",
+				"children": [
+					{
+						"id": 42,
+						"name": "React Hooks"
+					},
+					{
+						"id": 43,
+						"name": "Redux"
+					},
+					{
+						"id": 44,
+						"name": "Jest"
+					},
+					{
+						"id": 45,
+						"name": "Gatsby.js"
+					},
+					{
+						"id": 46,
+						"name": "Next.js"
+					},
+					{
+						"id": 47,
+						"name": "Animation Libraries",
+						"children": [
+							{
+								"id": 49,
+								"name": "Framer Motion"
+							}
+						]
+					},
+					{
+						"id": 50,
+						"name": "Component Libraries",
+						"children": [
+							{
+								"id": 52,
+								"name": "Material UI"
+							},
+							{
+								"id": 53,
+								"name": "Semantic UI React"
+							},
+							{
+								"id": 54,
+								"name": "React Bootstrap"
+							},
+							{
+								"id": 55,
+								"name": "Reactstrap"
+							}
+						]
+					},
+					{
+						"id": 56,
+						"name": "React Router"
+					}
+				]
+			},
+			{
+				"id": 57,
+				"name": "Angular"
+			},
+			{
+				"id": 58,
+				"name": "Vue.js",
+				"children": [
+					{
+						"id": 60,
+						"name": "Nuxt.js"
+					}
+				]
+			},
+			{
+				"id": 61,
+				"name": "jQuery"
+			},
+			{
+				"id": 62,
+				"name": "Webpack"
+			},
+			{
+				"id": 63,
+				"name": "Babel"
+			},
+			{
+				"id": 64,
+				"name": "WebRTC"
+			}
+		]
+	},
+	{
+		"id": 65,
+		"name": "Automated Tests",
+		"children": [
+			{
+				"id": 67,
+				"name": "App Performance Test",
+				"children": [
+					{
+						"id": 69,
+						"name": "Lighthouse"
+					}
+				]
+			},
+			{
+				"id": 70,
+				"name": "UI Testing",
+				"children": [
+					{
+						"id": 72,
+						"name": "Selenium"
+					}
+				]
+			},
+			{
+				"id": 73,
+				"name": "Unit Testing",
+				"children": [
+					{
+						"id": 75,
+						"name": "JavaScript",
+						"children": [
+							{
+								"id": 77,
+								"name": "Mocha"
+							},
+							{
+								"id": 78,
+								"name": "Chai"
+							},
+							{
+								"id": 79,
+								"name": "SinonJS"
+							}
+						]
+					},
+					{
+						"id": 80,
+						"name": "Python",
+						"children": [
+							{
+								"id": 82,
+								"name": "unittest"
+							},
+							{
+								"id": 83,
+								"name": "Nose"
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		"id": 84,
+		"name": "Database",
+		"children": [
+			{
+				"id": 86,
+				"name": "MongoDB",
+				"children": [
+					{
+						"id": 88,
+						"name": "Mongoose"
+					},
+					{
+						"id": 89,
+						"name": "MongoDB Atlas"
+					},
+					{
+						"id": 90,
+						"name": "MongoDB Stitch"
+					}
+				]
+			},
+			{
+				"id": 91,
+				"name": "SQLite"
+			},
+			{
+				"id": 92,
+				"name": "MySQL"
+			},
+			{
+				"id": 93,
+				"name": "PostgreSQL"
+			},
+			{
+				"id": 93,
+				"name": "SQL Server"
+			},
+		]
+	},
+	{
+		"id": 94,
+		"name": "DevOps (CI/CD)",
+		"children": [
+			{
+				"id": 96,
+				"name": "Jenkins",
+				"children": [
+					{
+						"id": 98,
+						"name": "Scripted Pipelines"
+					},
+					{
+						"id": 99,
+						"name": "Declarative Pipelines"
+					},
+					{
+						"id": 100,
+						"name": "Executor"
+					},
+					{
+						"id": 101,
+						"name": "cron"
+					}
+				]
+			},
+			{
+				"id": 101,
+				"name": "GitLab",
+				"children": [
+					{
+						"id": 98,
+						"name": "Worker"
+					},
+				]
+			},
+		]
+	},
+	{
+		"id": 102,
+		"name": "Build Tools",
+		"children": [
+			{
+				"id": 104,
+				"name": "Gradle"
+			},
+			{
+				"id": 105,
+				"name": "Maven"
+			},
+			{
+				"id": 106,
+				"name": "Ant"
+			},
+			{
+				"id": 107,
+				"name": "Webpack"
+			},
+			{
+				"id": 108,
+				"name": "Gulp"
+			}
+		]
+	},
+	{
+		"id": 109,
+		"name": "OS",
+		"children": [
+			{
+				"id": 111,
+				"name": "Windows",
+				"children": [
+					{
+						"id": 113,
+						"name": "Powershell"
+					},
+					{
+						"id": 114,
+						"name": "Batch Scripts"
+					}
+				]
+			},
+			{
+				"id": 115,
+				"name": "Linux",
+				"children": [
+					{
+						"id": 117,
+						"name": "Bash"
+					},
+					{
+						"id": 118,
+						"name": "CentOS",
+						"children": [
+							{
+								"id": 120,
+								"name": "yum"
+							}
+						]
+					},
+					{
+						"id": 121,
+						"name": "Ubuntu",
+						"children": [
+							{
+								"id": 123,
+								"name": "apt-get"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": 124,
+				"name": "Android"
+			},
+			{
+				"id": 125,
+				"name": "iOS"
+			}
+		]
+	},
+	{
+		"id": 102,
+		"name": "Code Collab",
+		"children": [
+			{
+				"id": 104,
+				"name": "VCS",
+				"children": [
+					{
+						"id": 105,
+						"name": "Git"
+					},
+					{
+						"id": 105,
+						"name": "Git"
+					},
+				]
+			},
+			{
+				"id": 105,
+				"name": "Kaban",
+				"children": [
+					{
+						"id": 105,
+						"name": "Jira"
+					},
+					{
+						"id": 105,
+						"name": "Trello"
+					}
+				]
+			},
+			{
+				"id": 106,
+				"name": "Agile",
+				"children": [
+					{
+						"id": 105,
+						"name": "Sprint Planning"
+					},
+					{
+						"id": 105,
+						"name": "Daily Stand Up"
+					},
+					{
+						"id": 105,
+						"name": "Sprint Review"
+					},
+					{
+						"id": 105,
+						"name": "Sprint Retrospective"
+					},
+				]
+			},
+			{
+				"id": 107,
+				"name": "Slack"
+			},
+			{
+				"id": 107,
+				"name": "Microsoft Teams"
+			},
+			{
+				"id": 108,
+				"name": "Discord"
+			}
+		]
+	},
+]
 
 function changeIds(obj) {
   let myId = 1
