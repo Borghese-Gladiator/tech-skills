612dcfe1e4857ab6881cb3f765b750ebc50e8b56
Timothy Shee <timothy_shee@hotmail.com>
2020-09-29 01:15:35 -0400

    Added button bars to SkillTree & changed tree icon sizes

--- src/components/SkillTree.js
+++ src/components/SkillTree.js
@@ -8,6 +8,65 @@ import TreeItem from '@material-ui/lab/TreeItem';
 import Collapse from '@material-ui/core/Collapse';
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
 
+// SearchTopicBar
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
+// ActionButtonBar
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const useSearchStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 300,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 5,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}))
+function SearchTopicBar(props) {
+  const classes = useSearchStyles()
+  const { searchAndOpen } = props
+
+  return (
+    <Paper component="form" className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search Topics"
+        inputProps={{ 'aria-label': 'search topics' }}
+      />
+      <IconButton className={classes.iconButton} aria-label="search" onClick={() => { console.log('onClick'); }}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
+
+function ActionButtonBar() {
+  return (
+    <ButtonGroup size="small" variant="text" color="primary" aria-label="small outlined button group" style={{padding: "3px"}}>
+      <Button>Collapse</Button>
+      <Button>Uncollapse</Button>
+    </ButtonGroup>
+  )
+}
+
 function MinusSquare(props) {
   return (
     <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
@@ -19,7 +78,7 @@ function MinusSquare(props) {
 
 function PlusSquare(props) {
   return (
-    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+    <SvgIcon fontSize="inherit" style={{ width: 18, height: 18 }} {...props}>
       {/* tslint:disable-next-line: max-line-length */}
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
     </SvgIcon>
@@ -28,7 +87,7 @@ function PlusSquare(props) {
 
 function CloseSquare(props) {
   return (
-    <SvgIcon className="close" fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+    <SvgIcon className="close" fontSize="inherit" style={{ width: 18, height: 18 }} {...props}>
       {/* tslint:disable-next-line: max-line-length */}
       <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
     </SvgIcon>
@@ -74,7 +133,18 @@ const useStyles = makeStyles({
     flexGrow: 1,
     maxWidth: 400,
   },
-});
+  panelHeading: {
+    backgroundColor: "#ededed",
+    borderRadius: "6px 6px 0 0",
+    color: "#363636",
+    fontSize: "1.25em",
+    fontWeight: 700,
+    lineHeight: 1.25,
+    padding: ".75em 1em",
+    marginTop: 0,
+    marginBottom: 0
+  }
+})
 
 export default function RecursiveTreeView(props) {
   const classes = useStyles();
@@ -104,7 +174,10 @@ export default function RecursiveTreeView(props) {
 
   return (
     <Container>
-      <h2>Topics</h2>
+      <h2 className={classes.panelHeading}>Topics</h2>
+      <SearchTopicBar />
+      <ActionButtonBar />
+      <br />
       <TreeView
         className={classes.root}
         defaultExpanded={["15"]}
