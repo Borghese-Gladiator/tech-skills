10aa6d13ca349be06b63197affe34a83d9c42f30
Timothy Shee <timothy_shee@hotmail.com>
2020-10-04 03:41:20 -0400

    Add navbar and scroll to top button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SkillPage from './pages/SkillPage'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
       <SkillPage />
       <Footer />
     </div>
